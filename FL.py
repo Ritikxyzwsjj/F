d11190f56af89fa7d1911230c314ec5a5cea77f9
Ritikxyzwsjj <ritikxyz44@gmail.com>
2025-04-24 22:37:38 +0530

    Update FL.py

--- FL.py
+++ FL.py
@@ -12,10 +12,10 @@ from telebot import types
 from dateutil.relativedelta import relativedelta
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7806976016:AAFNRC_r_quK6qJkri6J_LOMe1ZvxygaboU')
+bot = telebot.TeleBot('7972862104:AAEZ0h29LU-JsnMQWP4dfez7rQamXmYpcoY')
 
 # Admin user IDs
-admin_id = {"6434780221"}
+admin_id = {"8000733829"}
 
 # Files for data storage
 USER_FILE = "users.json"
@@ -340,7 +340,7 @@ def handle_attack(message):
         bot.register_next_step_handler(message, process_attack_details)
 
     else:
-        response = "⛔️ 𝗨𝗻𝗮𝘂𝘁𝗼𝗿𝗶𝘀𝗲𝗱 𝗔𝗰𝗰𝗲𝘀𝘀! ⛔️\n\n OWNER :- @LEGENDGROUP3OX !"
+        response = "⛔️ 𝗨𝗻𝗮𝘂𝘁𝗼𝗿𝗶𝘀𝗲𝗱 𝗔𝗰𝗰𝗲𝘀𝘀! ⛔️\n\n OWNER :- @rovin_vip !"
         bot.reply_to(message, response)
 
 def process_attack_details(message):
